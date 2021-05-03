f5aacf202ecfe6186d15ed1cbb1497a75e23fee1
1stDimension <23144389+1stDimension@users.noreply.github.com>
2021-05-03 23:07:38 +0200

    implementing new dunction

--- tekst.py
+++ tekst.py
@@ -1,36 +1,3 @@
 x1 = 4
-x2=x1
+# read x2
 print x2
-print x2 * x1
-x3 = x1 * x2
-print x3
-print "Bazinga"
-print "Bazinga"
-napis = "Jestem zmienna"
-print napis
-
-print "Liczby calkowite:"
-print 5 + 1
-print 100 / 20
-
-b= -3 - 2
-print b
-a= 10 + 2
-print a
-print ((a + b) * a) / ((int)2.0 * 2) #21
-print a - b
-print a * b
-print a / b
-
-print "Liczby rzeczywiste:"
-print 3.0 * 3.0     #9.0
-print (int)10.0 / 2 #5
-
-
-e= -3.0+1.0
-print e
-e= (real)5 * 3.0    #15.0
-print e
-d = 10.0 / 2.0      #5.0
-print d
-print e * d
