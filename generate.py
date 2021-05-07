7463baab2698f503effe0f17b8a208b16e1dea18
1stDimension <23144389+1stDimension@users.noreply.github.com>
2021-05-07 20:10:57 +0200

    generate.py after refactor

--- generate.py
+++ generate.py
@@ -1,10 +1,24 @@
+from functools import wraps
+
+
 class LLVMGenerator:
+    @__dec
     def __init__(self):
         self.header_text = ""
         self.main_text = ""
         self.reg = 1
 
-    @staticmethod
+    @__dec
+    def __dec(foo):
+        @wraps(foo)
+        def inner(*args, **kwargs):
+            val = foo(*args, **kwargs)
+            self.reg += 1
+            return val
+
+        return inner
+
+    @__dec
     def printf_i32(self, id):
         self.load_i32(self, id)
         self.main_text += (
@@ -14,9 +28,8 @@ class LLVMGenerator:
             + str(self.reg - 1)
             + ")\n"
         )
-        self.reg += 1
 
-    @staticmethod
+    @__dec
     def printf_undefined_i32(self, id):
         self.main_text += (
             "%"
@@ -25,9 +38,8 @@ class LLVMGenerator:
             + id
             + ")\n"
         )
-        self.reg += 1
 
-    @staticmethod
+    @__dec
     def printf_real(self, id):
         self.load_real(self, id)
         self.main_text += (
@@ -37,9 +49,8 @@ class LLVMGenerator:
             + str(self.reg - 1)
             + ")\n"
         )
-        self.reg += 1
 
-    @staticmethod
+    @__dec
     def printf_undefined_real(self, id):
         self.main_text += (
             "%"
@@ -48,196 +59,158 @@ class LLVMGenerator:
             + id
             + ")\n"
         )
-        self.reg += 1
 
-    @staticmethod
+    @__dec
     def printf_str(self):
-        # matched_lines = [line for line in self.main_text.split('\n') if "@"+id in line]
-        # print(matched_lines)
-        # self.main_text += ("%"+ str(self.reg)+ " = call i32 (i8*, ...) @printf(i8* getelementptr inbounds (["
-        #     + str(len(text))+ " x i8], ["+ str(len(text))+ " x i8]* @"+ id+ ", i32 0, i32 0))\n")
-        # self.reg += 1
-        self.header_text += (f"""@.str{self.reg} = private unnamed_addr constant [4 x i8] c"%s\\0A\\00"\n""")  # +2 bo dodajemy dwa znaki
-        self.main_text += (f"""%{self.reg} = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str{self.reg}, i32 0, i32 0), i8* %{self.reg -1})\n""")
-        self.reg += 1
+        self.header_text += f"""@.str{self.reg} = private unnamed_addr constant [4 x i8] c"%s\\0A\\00"\n"""  # +2 bo dodajemy dwa znaki
+        self.main_text += f"""%{self.reg} = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.str{self.reg}, i32 0, i32 0), i8* %{self.reg -1})\n"""
 
-
-
-
-    @staticmethod
+    @__dec
     def printf_undefined_str(self, id):
-        self.header_text += (f"""@.str{self.reg} = private unnamed_addr constant [{len(id)+2} x i8] c"{id}\\0A\\00"\n""")  # +2 bo dodajemy dwa znaki
-        self.main_text += (f"""%{self.reg} = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([{len(id)+2} x i8], [{len(id)+2} x i8]* @.str{self.reg}, i32 0, i32 0), i8* %{self.reg -1})\n""")
-        self.reg += 1
+        self.header_text += f"""@.str{self.reg} = private unnamed_addr constant [{len(id)+2} x i8] c"{id}\\0A\\00"\n"""  # +2 bo dodajemy dwa znaki
+        self.main_text += f"""%{self.reg} = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([{len(id)+2} x i8], [{len(id)+2} x i8]* @.str{self.reg}, i32 0, i32 0), i8* %{self.reg -1})\n"""
 
-
-
-    @staticmethod
+    @__dec
     def scanf_i32(self, id):
         self.main_text += f"%{self.reg} = call i32 (i8*, ...) @__isoc99_scanf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @strpi, i32 0, i32 0), i32* %{id})\n"
-        self.reg += 1
 
+    @__dec
     def scanf_double(self, id):
         self.main_text += f"%{self.reg} = call i32 (i8*, ...) @__isoc99_scanf(i8* getelementptr inbounds ([5 x i8], [5 x i8]* @strpd, i32 0, i32 0), double* %{id})\n"
-        self.reg += 1
 
-    @staticmethod
     def declare_i32(self, id):
         self.main_text += f"%{id} = alloca i32\n"
 
-    @staticmethod
     def declare_real(self, id):
         self.main_text += f"%{id} = alloca double\n"
-    
-    @staticmethod
-    def declare_str(self, id,text):
+
+    def declare_str(self, id, text):
         self.main_text += f"%{id} = alloca [{len(text)-1} x i8]\n"
 
-    @staticmethod
     def assign_i32(self, id, value):
         self.main_text += f"store i32 {value}, i32* %{id}\n"
 
-    @staticmethod
     def assign_real(self, id, value):
         self.main_text += f"store double {value}, double* %{id}\n"
 
-    @staticmethod
+    @__dec
     def assign_str(self, id, value):
         self.header_text += f"""@{id} = private unnamed_addr constant [{len(value)} x i8] c{value}\\00"\n"""
-        self.main_text += "%" +str(self.reg)+"= bitcast ["+str(len(value))+" x i8]* %"+id+" to i8*\n"
+        self.main_text += (
+            "%"
+            + str(self.reg)
+            + "= bitcast ["
+            + str(len(value))
+            + " x i8]* %"
+            + id
+            + " to i8*\n"
+        )
         self.main_text += f"call void @llvm.memcpy.p0i8.p0i8.i64(i8* %{self.reg}, i8* getelementptr inbounds ([{len(value)} x i8], [{len(value)} x i8]* @{id}, i32 0, i32 0), i64 {len(value)}, i32 1, i1 false)\n"
-        self.reg+=1
 
-    @staticmethod
     def assign_array_i32(self, id, size):
         ty = "i32"
         self.main_text += f"%{id} = alloca [{size} x {ty}]\n"
 
-    @staticmethod
     def assign_array_double(self, id, size):
         ty = "double"
         self.main_text += f"%{id} = alloca [{size} x {ty}]\n"
 
-    @staticmethod
+    @__dec
     def store_array_i32(self, id, offset, value, size):
         ty = "i32"
         self.main_text += f"%{self.reg} = getelementptr inbounds [{size} x {ty}], [{size} x {ty}]* %{id}, i32 0, i32 {offset}\n"
         self.main_text += f"store {ty} {value}, {ty}* %{self.reg}\n"
-        self.reg += 1
 
-    @staticmethod
+    @__dec
     def store_array_double(self, id, offset, value, size):
         ty = "double"
         self.main_text += f"%{self.reg} = getelementptr inbounds [{size} x {ty}], [{size} x {ty}]* %{id}, i32 0, i32 {offset}\n"
         self.main_text += f"store {ty} {value}, {ty}* %{self.reg}\n"
-        self.reg += 1
 
-    @staticmethod
+    @__dec
     def load_i32(self, id):
-        self.main_text += "%" + str(self.reg) + " = load i32, i32* %" + id + "\n"
-        self.reg += 1
+        self.main_text += f"%{self.reg} = load i32, i32* %{id}\n"
 
-    @staticmethod
+    @__dec
     def load_real(self, id):
-        self.main_text += "%" + str(self.reg) + " = load double, double* %" + id + "\n"
-        self.reg += 1
+        self.main_text += f"%{self.reg} = load double, double* %{id}\n"
 
-    @staticmethod
+    @__dec
     def load_str(self, id, text1):
-        self.main_text += (f"%{self.reg} = getelementptr inbounds [{len(text1)-1} x i8], [{len(text1)-1} x i8]* %{id}, i32 0, i32 0\n")
-        self.reg +=1
-    @staticmethod
+        self.main_text += f"%{self.reg} = getelementptr inbounds [{len(text1)-1} x i8], [{len(text1)-1} x i8]* %{id}, i32 0, i32 0\n"
+
+    @__dec
     def load_array_i32(self, id, offset, size):
         ty = "i32"
         self.main_text += f"%{self.reg} = getelementptr inbounds [{size} x {ty}], [{size} x {ty}]* %{id}, i32 0, i32 {offset}\n"
         self.reg += 1
         self.main_text += f"%{self.reg} = load {ty}, {ty}* %{self.reg-1}\n"
-        self.reg += 1
 
-    @staticmethod
+    @__dec
     def load_array_double(self, id, offset, size):
         ty = "double"
         self.main_text += f"%{self.reg} = getelementptr inbounds [{size} x {ty}], [{size} x {ty}]* %{id}, i32 0, i32 {offset}\n"
         self.reg += 1
         self.main_text += f"%{self.reg} = load {ty}, {ty}* %{self.reg-1}\n"
-        self.reg += 1
-    @staticmethod
+
+    @__dec
     def add_i32(self, val1, val2):
         self.main_text += (
             "%" + str(self.reg) + " = add i32 " + val1 + ", " + val2 + "\n"
         )
-        self.reg += 1
 
-    @staticmethod
+    @__dec
     def add_real(self, val1, val2):
         self.main_text += (
             "%" + str(self.reg) + " = fadd double " + val1 + ", " + val2 + "\n"
         )
-        self.reg += 1
 
-    @staticmethod
+    @__dec
     def add_str(self, val1, val2):
-        self.main_text += (f"%{self.reg} = call i8* @strcat(i8* %{self.reg-2}, i8* %{self.reg-1})\n")
-        self.reg +=1
+        self.main_text += (
+            f"%{self.reg} = call i8* @strcat(i8* %{self.reg-2}, i8* %{self.reg-1})\n"
+        )
 
-
-    @staticmethod
+    @__dec
     def sub_i32(self, val1, val2):
         self.main_text += (
             "%" + str(self.reg) + " = sub i32 " + val2 + ", " + val1 + "\n"
         )
-        self.reg += 1
 
-    @staticmethod
+    @__dec
     def sub_real(self, val1, val2):
         self.main_text += (
             "%" + str(self.reg) + " = fsub double " + val2 + ", " + val1 + "\n"
         )
-        self.reg += 1
 
-    @staticmethod
+    @__dec
     def mul_i32(self, val1, val2):
-        self.main_text += (
-            "%" + str(self.reg) + " = mul i32 " + val1 + ", " + val2 + "\n"
-        )
-        self.reg += 1
+        self.main_text += f"%{self.reg} = mul i32 {val1}, {val2}\n"
 
-    @staticmethod
+    @__dec
     def mul_real(self, val1, val2):
-        self.main_text += (
-            "%" + str(self.reg) + " = fmul double " + val1 + ", " + val2 + "\n"
-        )
-        self.reg += 1
+        self.main_text += f"%{self.reg} = fmul double {val1}, {val2}\n"
 
-    @staticmethod
+    @__dec
     def div_i32(self, val1, val2):
-        self.main_text += (
-            "%" + str(self.reg) + " = sdiv i32 " + val2 + ", " + val1 + "\n"
-        )
-        self.reg += 1
+        self.main_text += f"%{self.reg} = sdiv i32 {val2}, {val1}\n"
 
-    @staticmethod
+    @__dec
     def div_real(self, val1, val2):
-        self.main_text += (
-            "%" + str(self.reg) + " = fdiv double " + val2 + ", " + val1 + "\n"
-        )
-        self.reg += 1
+        self.main_text += f"%{self.reg} = fdiv double {val2}, {val1}\n"
 
-    @staticmethod
     def itostr(self, id):
         pass
 
-    @staticmethod
+    @__dec
     def sitofp(self, id):
         self.main_text += f"%{self.reg} = sitofp i32 {id} to double\n"
-        self.reg += 1
 
-    @staticmethod
+    @__dec
     def fptosi(self, id):
         self.main_text += f"%{self.reg} = fptosi double {id} to i32\n"
-        self.reg += 1
 
-    @staticmethod
+    @__dec
     def generate(self):
         text = ""
         text += "declare i32 @printf(i8*, ...)\n"
