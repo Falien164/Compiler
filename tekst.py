fe9105d42153d2007d4db4779fbc8d7ceec6457e
1stDimension <23144389+1stDimension@users.noreply.github.com>
2021-05-27 22:49:48 +0200

    better example

--- tekst.py
+++ tekst.py
@@ -1,13 +1,10 @@
 
-fn main() -> int {
-    x = 3 #none()
+fn foo() -> int {
+    return 2
+}
+
+ fn main() -> int {
+    x =2# foo()
     print x
-    tab = {0,1,2,3,4,5,6}
-    l = 7
-    i = 0
-    while (i < l){
-        print tab[i]
-        i = i + 1
-    }
     return 0
 }
