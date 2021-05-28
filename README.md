25ac345c01573f911a68e8a012d54c4f023acd75
1stDimension <23144389+1stDimension@users.noreply.github.com>
2021-05-28 13:57:57 +0200

    dbug

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Compiler
 My own compiler using ANTLR and LLVM
+
+## Zmienne globalne
+
+Zmienne globalne too wszystkie zmienne które zostały zadeklarowane poza ciałem funkcji.
+
+Aby uzyskać dostęp do zmiennej globalnej należy użyć operatora "@"
+
+```
+zmienna = 0
+fn main() -> int {
+  
+    @zmienna = 2.0
+    return 0
+}
+
+
+```
--- tekst.py
+++ tekst.py
@@ -7,12 +7,23 @@ fn condition(int a) -> int {
     if ( x == 2 and y > 5 ){
         qwe1 = "spelniony warunek 1"
         print qwe1
+        if ( y == 6 or y == 8){
+            qwe10 = "y = 6 lub 8"
+            print qwe10 
+        }
     } else if ( x ==  2 and y < 5){
         qwe2 = "spelniony warunek 2"
         print qwe2
+    } else if (x == 2 and y == 5) {
+        qwe5 = "spelniony warunek 3"
+        print qwe5
+    } else {
+        qwe4 = "zodyn warunek nie spelniony"
+        print qwe4
     }
-    return x
+    return 0
 }
+
 
 
 glob = 2
@@ -34,10 +45,16 @@ fn main() -> int {
     print glob
 
     a = 99
-    cond = condition(a)
+    #cond = condition(a)
 
-
-
+    tab = {0, 0, 0, 0, 0, 0}
+    i = 0
+    
+    while (i < 6){
+        tab[i] = i
+        print tab[i]
+        i = i + 1
+    }
+    
     return 0
 }
-
